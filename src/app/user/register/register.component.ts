c7b213623c9aa64ea13c2815b7e012f67c365d90
risranjan <risranjan@deloitt.com>
2024-01-02 22:45:48 +0530

    Finishing custom validators

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IUser from 'src/app/models/user.model';
 import { AuthService } from './../../services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from './../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,7 @@ import { AuthService } from './../../services/auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
   inSubmission = false;
   // registerForm = new FormGroup({
   //   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -34,7 +36,11 @@ export class RegisterComponent {
   // });
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -54,14 +60,17 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your account is being created.';
   alertColor = 'blue';
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
